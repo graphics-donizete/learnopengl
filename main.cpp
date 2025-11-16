acd19ddfbc3677fe3026960ba92fdfa135a3816f
Donizete Vida <donizetevida1998@outlook.com>
2025-11-16 16:27:51 +0100

    Add Context config object

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@
 
 int main(int, char **)
 {
-    sf::Window window(sf::VideoMode({800, 600}), "OpenGL");
+    sf::ContextSettings settings{
+        .depthBits = 24,
+        .stencilBits = 8,
+        .antiAliasingLevel = 4,
+        .majorVersion = 3,
+        .minorVersion = 3,
+        .attributeFlags = sf::ContextSettings::Attribute::Core,
+    };
+
+    sf::Window window(sf::VideoMode({800, 600}), "OpenGL", sf::Style::Default, sf::State::Windowed, settings);
     glEnable(GL_TEXTURE_2D);
 
     while (window.isOpen())
