05953479f8dd716177abda3d68e196ce8cdfc649
Donizete Vida <donizetevida1998@outlook.com>
2025-11-16 16:11:29 +0100

    Create SFML Window

--- main.cpp
+++ main.cpp
@@ -2,9 +2,22 @@
 
 #include <GL/gl.h>
 
+#include <SFML/Window.hpp>
+
 int main(int, char **)
 {
-    std::cout << "Hello, from learnopengl!\n";
+    sf::Window window(sf::VideoMode({800, 600}), "OpenGL");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+    }
 
     glBlendEquation(1);
 }
